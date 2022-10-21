2ef3f245ee0c21b85a1b5844c00de02c04b64258
RAPHAELA_MARQUES_DOS_SANTOS <71901078@SIGMAB16.BARROCA>
2022-10-20 21:51:10 -0300

    Fix autowired

--- src/main/java/br/com/plataformacursos/PlataformaDeCursos.java
+++ src/main/java/br/com/plataformacursos/PlataformaDeCursos.java
@@ -3,7 +3,7 @@ package br.com.plataformacursos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"br.com.plataformacursos.repository"})
 public class PlataformaDeCursos {
 
 	public static void main(String[] args) {
