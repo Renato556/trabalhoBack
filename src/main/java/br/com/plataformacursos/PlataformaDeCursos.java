0bbfef15d12d4a26deb847402105b31be5cd2007
unknown <marcosga.correa@gmail.com>
2022-11-17 17:24:13 -0300

    init database

--- src/main/java/br/com/plataformacursos/PlataformaDeCursos.java
+++ src/main/java/br/com/plataformacursos/PlataformaDeCursos.java
@@ -2,6 +2,10 @@ package br.com.plataformacursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication(scanBasePackages = {"br.com.plataformacursos.repository"})
 public class PlataformaDeCursos {
@@ -9,5 +13,4 @@ public class PlataformaDeCursos {
 	public static void main(String[] args) {
 		SpringApplication.run(PlataformaDeCursos.class, args);
 	}
-
 }
